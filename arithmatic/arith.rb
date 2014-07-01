883b31320bddd1e39b1a59bbc1266cca99495a15
deepankar <deepankar@railsfactory.org>
2014-07-01 18:00:45 +0530

    arithmatic operations

---
+++ arithmatic/arith.rb
@@ -0,0 +1,7 @@
+puts "Enter two numbers"
+i = gets.chomp.to_i
+j = gets.chomp.to_i
+puts "addition : #{i+j}"
+puts "substraction : #{i-j}"
+puts "Multiplication : #{i*j}"
+puts "Division : #{i.to_f/j}"
