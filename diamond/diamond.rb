b65a58fd5a4b40763407db4b9eddb8aeb9650e4e
deepankar <deepankar@railsfactory.org>
2014-07-02 15:23:06 +0530

    Forming a diamond shapped pattern

---
+++ diamond/diamond.rb
@@ -0,0 +1,13 @@
+puts " Enter the number of rows"
+w = gets.chomp.to_i
+1.upto(w) do |i|                          
+  spaces = " " * (w-i)
+  stars   = "." * (i*2 - 1) 
+  puts "" + spaces + stars + spaces + ""
+end
+(w).downto(1) do |i|
+  spaces = " " * (w-i)
+  stars   = "." * (i*2 - 1)
+  puts   spaces + stars + spaces 
+end
+
