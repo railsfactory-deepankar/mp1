6d05cd9a7ee168ff94bae4b43e21a4835181f6b5
deepankar <deepankar@railsfactory.org>
2014-07-07 17:54:57 +0530

    Changing string cases

---
+++ change_case/change_case.c
@@ -0,0 +1,13 @@
+
+
+#include <stdio.h>
+#include <string.h>
+ 
+int main()
+{
+    char string[] = "Strlwr in C";
+ 
+    printf("%s\n",strlwr(string));
+ 
+    return  0;
+}
---
+++ change_case/change_case.rb
@@ -0,0 +1,7 @@
+puts "Enter a string"
+i = gets.chomp.to_s
+d=i.downcase
+u=i.upcase
+puts "String in downcase: #{d}"
+puts "String in upcase: #{u}"
+
