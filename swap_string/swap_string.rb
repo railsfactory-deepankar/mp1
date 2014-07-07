250b780d16aa32f744f6e288aa2b7ed330538262
deepankar <deepankar@railsfactory.org>
2014-07-07 18:09:32 +0530

    Swapping of two string

---
+++ swap_string/swap_string.rb
@@ -0,0 +1,7 @@
+puts "Enter the two strings"
+i = gets.chomp.to_s
+j = gets.chomp.to_s
+puts "Strings before swapping"
+puts "First string : #{i}"
+puts "Second string : #{j}"
+puts "After swapping: \n First String : #{j} \n Second String : #{i}"
