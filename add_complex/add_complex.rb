db6c27afa8c0992e037bed8804cc03ee68962200
deepankar <deepankar@railsfactory.org>
2014-07-08 10:30:55 +0530

    Adding two complex numbers

---
+++ add_complex/add_complex.rb
@@ -0,0 +1,9 @@
+puts"Enter a and b where a+ib is the first complex number"
+a = gets.chomp.to_i
+b = gets.chomp.to_i
+puts "Enter c and d where c+id is the second complex number"
+c = gets.chomp.to_i
+d = gets.chomp.to_i
+s = a+c
+s1 = b+d
+puts "sum of two complex number is : #{s}+i#{s1}"
